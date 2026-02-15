4025aacec42851ca1129034eaf3caf05bc519ec0
Khoi-it <22130127@st.hcmuaf.edu.vn>
2026-02-15 23:29:49 +0700

    Fix 404: Disable image optimization and rename fonts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     images: {
+        unoptimized: true,
         remotePatterns: [
             {protocol: 'https', hostname: 'lh3.googleusercontent.com'}, // Cho ảnh Google Drive
             {protocol: 'https', hostname: 'drive.google.com'},          // Cho link Google Drive
